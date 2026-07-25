fix units

exph/debye.py
@@ -96,12 +96,12 @@ def compute_debye_exph(SAVE_folder, BSE_dir, elph_file, nexc):
     kvec = np.zeros(3)
     Ak = excdb.get_Akcv()
     exph_debye = exciton_X_matelem(kvec, kvec, Ak, Ak, Dwaller, kpoints)
-    return exph_debye.transpose(0,2,1).reshape(natom,3,3,nexc,nexc)
+    return exph_debye.transpose(0,2,1).reshape(natom,3,3,nexc,nexc) * 0.25 ## factor from ry to ha
 
 
-SAVE_folder = 'SAVE'
-BSE_dir = 'GW_BSE'
-elph_file = 'ndb.elph'
-debye_exph = compute_debye_exph(SAVE_folder, BSE_dir, elph_file, nexc=10)
-np.save('debye_exph',debye_exph)
-print(np.sum(np.abs(debye_exph)**2))
+# SAVE_folder = 'SAVE'
+# BSE_dir = 'GW_BSE'
+# elph_file = 'ndb.elph'
+# debye_exph = compute_debye_exph(SAVE_folder, BSE_dir, elph_file, nexc=10)
+# np.save('debye_exph',debye_exph)
+# print(np.sum(np.abs(debye_exph)**2))

exph/raman.py
@@ -620,8 +620,8 @@ def compute_two_ph_debye_exc(ome_light,
     Nqpts, nmode = ph_freq_q.shape
     N_exc = ex_dip.shape[1]
 
-    omegaq_inv  = np.where(ph_freq_q >  2e-5, 1.0 / np.sqrt(2.0 * ph_freq_q), 0.0)
-    omegamq_inv = np.where(ph_freq_mq > 2e-5, 1.0 / np.sqrt(2.0 * ph_freq_mq), 0.0)
+    omegaq_inv  = np.where(ph_freq_q >  1e-5, 1.0 / np.sqrt(2.0 * ph_freq_q), 0.0)
+    omegamq_inv = np.where(ph_freq_mq > 1e-5, 1.0 / np.sqrt(2.0 * ph_freq_mq), 0.0)
 
     pol_vec_mq_tmp = pol_vec_mq * omegamq_inv[:,:,None,None]
     pol_vec_q_tmp = pol_vec_q  * omegaq_inv[:,:,None,None]